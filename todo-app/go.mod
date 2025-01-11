ba4cd409de7e41c25acc1aa59a5094cd50181a66
roshankaranth <roshankaranth@gmail.com>
2025-01-11 21:32:53 +0530

    time difference

--- todo-app/go.mod
+++ todo-app/go.mod
@@ -1,3 +1,5 @@
 module github.com/roshankaranth/todo-app
 
 go 1.23.4
+
+require github.com/mergestat/timediff v0.0.3 // indirect
--- todo-app/utils.go
+++ todo-app/utils.go
@@ -5,8 +5,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 	"text/tabwriter"
+	"time"
+
+	"github.com/mergestat/timediff"
 )
 
 func AddTask() {
@@ -19,6 +23,7 @@ func ListTasks() {
 
 	r := csv.NewReader(strings.NewReader(string(data)))
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+	var field bool
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -27,8 +32,14 @@ func ListTasks() {
 		if err != nil {
 			panic(err)
 		}
+		if field {
 
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t\n", record[0], record[1], record[2], record[3])
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t\n", record[0], record[1], calculateTime(record[2]), record[3])
+		} else {
+			fmt.Fprintf(w, "ID\tTask\tCreated\tDone\t\n")
+		}
+
+		field = true
 
 	}
 	w.Flush()
@@ -50,4 +61,31 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func calculateTime(taskCreated string) string {
+	//2024-07-27T16:45:19-05:00
+	tdate, ttime, _ := strings.Cut(taskCreated, "T")
+	parts := strings.Split(ttime, "-")
+	tunits := strings.Split(parts[0], ":")
+	hour, _ := strconv.Atoi(tunits[0])
+	min, _ := strconv.Atoi(tunits[1])
+	sec, _ := strconv.Atoi(tunits[2])
+
+	dunits := strings.Split(tdate, "-")
+	dunits_now := strings.Split(time.Now().Format("2006-01-02"), "-")
+
+	tyear, _ := strconv.Atoi(dunits[0])
+	tmonth, _ := strconv.Atoi(dunits[1])
+	tday, _ := strconv.Atoi(dunits[2])
+	nyear, _ := strconv.Atoi(dunits_now[0])
+	nmonth, _ := strconv.Atoi(dunits_now[1])
+	nday, _ := strconv.Atoi(dunits_now[2])
+
+	hour += (nyear-tyear)*8760 + (nmonth-tmonth)*730 + (nday-tday)*24
+
+	ftime := timediff.TimeDiff(time.Now().Add(time.Duration(-hour) * time.Hour).Add(time.Duration(-min) * time.Minute).Add(time.Duration(-sec) * time.Second))
+
+	return ftime
+
 }
